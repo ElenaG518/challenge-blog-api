d09b6437721808f7820c94ed440d79e75493e518
Elena Granados <patty518@yahoo.com>
2018-09-06 15:30:17 -0700

    created schema for models.js

--- models.js
+++ models.js
@@ -1,62 +1,40 @@
-const uuid = require('uuid');
+"use strict";
+
+const mongoose = require("mongoose");
+
+// this is our schema to represent a restaurant
+const blogSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { 
+      firstName: type: String, required: true,
+      lastName: type: String, required: true }
+});
 
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
-}
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the address object
+// we're storing in Mongo.
+blogSchema.virtual("authorFullNameString").get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    console.log('crating new blog item');
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
-  }
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorFullNameString
+  };
 };
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const BlogPosts = mongoose.model("BlogPosts", blogSchema);
 
-
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = { BlogPosts };
