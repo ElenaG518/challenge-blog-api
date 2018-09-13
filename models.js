4b3ad0383f50d9004101b431a08be0b658a548e3
Elena Granados <patty518@yahoo.com>
2018-09-12 20:11:05 -0700

    added author endpoints

--- models.js
+++ models.js
@@ -1,41 +1,75 @@
 "use strict";
 
 const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-// this is our schema to represent a restaurant
-const blogSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { 
-      firstName: { type: String, required: true },
-      lastName: { type: String, required: true }
+const authorSchema = mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  userName: {
+    type: 'string',
+    unique: true
   }
 });
 
+const commentSchema = mongoose.Schema({ content: 'string' });
+
+// this is our schema to represent a restaurant
+const blogPostSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+  comments: [commentSchema]
+});
+
+
+blogPostSchema.pre('findOne', function(next) {
+  this.populate('author');
+  next();
+});
+
+blogPostSchema.pre('find', function(next) {
+  this.populate('author');
+  next();
+});
+
+authorSchema.virtual("name").get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`;
+});
+
+authorSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    name: name,
+    userName: this.userName
+  };
+};
 
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
 // allow us to define properties on our object that manipulate
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
-blogSchema.virtual("authorFullNameString").get(function() {
+blogPostSchema.virtual("authorName").get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
 // exposes *some* of the fields we want from the underlying data
-blogSchema.methods.serialize = function() {
+blogPostSchema.methods.serialize = function() {
   return {
     id: this._id,
     title: this.title,
     content: this.content,
-    author: this.authorFullNameString
+    author: this.authorName,
+    comments: this.comments
   };
 };
 
+const Author = mongoose.model('Author', authorSchema);
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
-const BlogPost = mongoose.model("BlogPost", blogSchema);
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
 
-module.exports = { BlogPost };
+module.exports = { Author, BlogPost }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const morgan = require('morgan');
 const mongoose = require("mongoose");
 
 // Mongoose internally uses a promise-like object,
@@ -10,10 +11,117 @@ mongoose.Promise = global.Promise;
 // config.js is where we control constants for entire
 // app like PORT and DATABASE_URL
 const { PORT, DATABASE_URL } = require("./config");
-const { BlogPost } = require("./models");
+const { Author, BlogPost } = require("./models");
+// const { Author } = require("./models");
 
 const app = express();
+
+app.use(morgan('common'));
 app.use(express.json());
+
+
+app.get('/authors', (req, res) => {
+  Author
+    .find()
+    .then(authors => {
+      res.json({
+        authors: authors.map(author => author.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+app.post("/authors", (req, res) => {
+  const requiredFields = ["firstName", "lastName", "userName"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  };
+
+  Author
+    .findOne({ userName: req.body.userName })
+    .then(author => {
+      if (author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName
+          })
+          .then(author => res.status(201).json(author.serialize()))
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+app.put("authors/:id", (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(  (req.params.id) &&  (req.body.id) && (req.params.id === req.body.id) ) ) {
+    const message =
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ["firstName", "lastName", "userName" ];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Author
+    .findOne({ userName: updated.userName || '' , _id: { $ne: req.params.id } })
+    .then(author => {
+      if(author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+          .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+          .then(updatedAuthor=> res.status(201).json(updatedAuthor.serialize()))
+          .catch(err => res.status(500).json({ message: "Internal server error" }));
+      }
+    });
+});    
+
+
+app.delete("/author/:id", (req, res) => {
+  Author.findByIdAndRemove(req.params.id)
+    .then(author => res.status(204).end())
+    .catch(err => res.status(500).json({message: "Internal server error"}));
+
+});    
+
+
 
 app.get("/posts", (req, res) => {
   // res.sendFile(__dirname + '/views/index.html');
@@ -21,6 +129,7 @@ app.get("/posts", (req, res) => {
   // success callback: for each restaurant we got back, we'll
     // call the `.serialize` instance method we've created in
     // models.js in order to only expose the data we want the API return.    
+    
     .then(blogs => {
       res.json({
         blogs: blogs.map(blog => blog.serialize())
@@ -38,15 +147,23 @@ app.get("/posts/:id", (req, res) => {
     // this is a convenience method Mongoose provides for searching
     // by the object _id property
     .findById(req.params.id)
-    .then(blog => res.json(blog.serialize()))
+    .then(post => {
+      post.comments.push({ content: 'a comment on that last comment' });
+      post.save();
+    })
+    .then(post => {
+      res.json(post.serialize())
+    })
     .catch(err => {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     });
 });
 
+
+
 app.post("/posts", (req, res) => {
-  const requiredFields = ["title", "content", "author"];
+  const requiredFields = ["title", "content", "author_id"];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -56,17 +173,33 @@ app.post("/posts", (req, res) => {
     }
   }
 
-  BlogPost.create({
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author
-  })
-    .then(blog=> res.status(201).json(blog.serialize()))
+  Author.findById(req.body.author_id)
+    .then(author => {
+      if (author) {
+        BlogPost.create({
+          title: req.body.title,
+          content: req.body.content,
+          author: req.body.id
+        })
+        .then( blog => res.status(201).json(blog.serialize()))
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({ message: "Internal server error" });
+        });
+      }
+      else { 
+        const message = "Author not found";
+        console.error(message);
+        res.status(400).send(message);
+      }
+     }) 
     .catch(err => {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     });
-});
+  });
+
+
 
 app.put("/posts/:id", (req, res) => {
   // ensure that the id in the request path and the one in request body match
@@ -82,7 +215,7 @@ app.put("/posts/:id", (req, res) => {
   // if the user sent over any of the updatableFields, we udpate those values
   // in document
   const toUpdate = {};
-  const updateableFields = ["title", "content", "author" ];
+  const updateableFields = ["title", "content" ];
 
   updateableFields.forEach(field => {
     if (field in req.body) {
@@ -92,10 +225,13 @@ app.put("/posts/:id", (req, res) => {
 
   BlogPost
     // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate }, { new: true })
     .then(blog => res.status(201).json(blog.serialize()))
     .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
+
+
+
 
 app.delete("/posts/:id", (req, res) => {
   BlogPost.findByIdAndRemove(req.params.id)
